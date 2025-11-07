10a827125cc64fa086112392ee64d304548b458c
pedromattar42 <pedromattar42@gmail.com>
2025-11-07 11:15:56 -0300

    feat(ListComponent): implementa listagem de tarefas de forma fixa usando DaisyUi e Tailwind

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+interface Task {
+  title: string;
+  isCompleted: boolean;
+}
 
 @Component({
   selector: 'app-list',
@@ -8,4 +13,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {}
+export class ListComponent {
+  tasks = signal<Task[]>([
+    { title: 'Tarefa 1', isCompleted: false },
+    { title: 'Tarefa 2', isCompleted: false },
+    { title: 'Tarefa 3', isCompleted: false },
+    { title: 'Tarefa 4', isCompleted: true },
+    { title: 'Tarefa 5', isCompleted: true },
+    { title: 'Tarefa 6', isCompleted: true },
+  ])
+  completedTasks = computed(() => this.tasks().filter((task) => task.isCompleted))
+  pendingTasks = computed(() => this.tasks().filter((task) => !task.isCompleted))
+
+}
