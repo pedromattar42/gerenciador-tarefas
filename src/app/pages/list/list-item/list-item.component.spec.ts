336d99399c5df871eb55b3003451fdf010efe8f4
pedromattar42 <pedromattar42@gmail.com>
2025-11-09 17:00:55 -0300

    test(ListItemComponent): testa o título da tarefa

---
+++ src/app/pages/list/list-item/list-item.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ListItemComponent } from './list-item.component';
+import { Task } from 'src/app/shared/interfaces/task.interface';
+import { TestHelper } from '@testing/helpers/test-helper';
+
+describe('ListItemComponent', () => {
+  let fixture: ComponentFixture<ListItemComponent>;
+  let testHelper: TestHelper<ListItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ListItemComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ListItemComponent);
+    testHelper = new TestHelper(fixture);
+  });
+
+  it('deve renderizar o título da tarefa', () => {
+    const fakeTask: Task = { title: 'Nome da tarefa', isCompleted: false };
+    fixture.componentRef.setInput('task', fakeTask);
+    
+    fixture.detectChanges();
+
+    const text = testHelper.getTextContentByTestId('list-item-task-title');
+
+    expect(text).toBe(fakeTask.title);
+  });
+});
