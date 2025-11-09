183c3e2689f953b4080895b1f1b9f4256b794921
pedromattar42 <pedromattar42@gmail.com>
2025-11-09 17:00:27 -0300

    feat(ListItemComponent): implementa template do componente

---
+++ src/app/pages/list/list-item/list-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Task } from 'src/app/shared/interfaces/task.interface';
+
+@Component({
+  selector: 'app-list-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list-item.component.html',
+  styleUrl: './list-item.component.scss',
+})
+export class ListItemComponent {
+  task = input.required<Task>();
+}
