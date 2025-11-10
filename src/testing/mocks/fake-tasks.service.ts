84afbe1153c249ebdfb6da373c78e301f317efa7
pedromattar42 <pedromattar42@gmail.com>
2025-11-10 11:28:05 -0300

    test(FakeTasksService): adiciona método patch

--- src/testing/mocks/fake-tasks.service.ts
+++ src/testing/mocks/fake-tasks.service.ts
@@ -4,4 +4,5 @@ import { TasksService } from 'src/app/shared/services/tasks/tasks.service';
 export class FakeTasksService implements TasksService {
   httpClient!: HttpClient;
   getAll = jest.fn();
+  patch = jest.fn();
 }
